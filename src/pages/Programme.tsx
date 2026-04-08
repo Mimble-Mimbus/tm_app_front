5bf73905eee23d376a87840cc1d5822370a8a475
Miriwar <remi.rochet34@gmail.com>
2026-04-08 19:12:54 +0200

    Ajout Page Programme

---
+++ src/pages/Programme.tsx
@@ -0,0 +1,10 @@
+import { IonContent, IonPage } from "@ionic/react";
+import { FC } from "react";
+
+const Programme: FC = () => {
+  return (<IonPage className="middle">
+      <p>Voici le programme du Weekend</p>
+  </IonPage>)
+}
+
+export default Programme
